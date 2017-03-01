26530f631e9f117f591ba6e1a6068eff1a075e39
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-01 23:08:53 +0200

    Update tests.py

--- data_structures/linked_list/problems/find_pattern_in_linked_list/py/tests.py
+++ data_structures/linked_list/problems/find_pattern_in_linked_list/py/tests.py
@@ -18,27 +18,126 @@ def TestMain(sol, log=sys.stdout, doNotLogPassed=True) -> bool:
   
     def TestPredefined(sol, log, doNotLogPassed=True) -> bool:        
         ARGS_EXPECTED_PAIRS = (
-            # Either list is null.
-            ((LinkedList.ConvertArrayToLinkedList(),            LinkedList.ConvertArrayToLinkedList(), ),           -1),
-            ((LinkedList.ConvertArrayToLinkedList('hay'),       LinkedList.ConvertArrayToLinkedList(), ),           -1),
-            ((LinkedList.ConvertArrayToLinkedList(),            LinkedList.ConvertArrayToLinkedList('hay'), ),      -1),
+            # Both lists are null.
+            (
+                (LinkedList.ConvertArrayToLinkedList(), 
+                 LinkedList.ConvertArrayToLinkedList(), ),
+                 -1
+            ),
+
+            # Pattern is null.
+            (
+                (LinkedList.ConvertArrayToLinkedList('hay'),       
+                 LinkedList.ConvertArrayToLinkedList(), ),           
+                 -1
+            ),
+
+            # Source is null.
+            (
+                (LinkedList.ConvertArrayToLinkedList(),            
+                 LinkedList.ConvertArrayToLinkedList('hay'), ),      
+                 -1
+            ),
+
+             # Needle longer than the haystack.
+            (
+                (LinkedList.ConvertArrayToLinkedList('ha'),        
+                 LinkedList.ConvertArrayToLinkedList('hay'), ),      
+                 -1
+            ),
+
+             # Needle not in the haystack.
+            (
+                (LinkedList.ConvertArrayToLinkedList('haystack'),  
+                 LinkedList.ConvertArrayToLinkedList('needle'), ),   
+                 -1
+            ),
 
             # Needle not in the haystack.
-            ((LinkedList.ConvertArrayToLinkedList('ha'),        LinkedList.ConvertArrayToLinkedList('hay'), ),      -1),
-            ((LinkedList.ConvertArrayToLinkedList('haystack'),  LinkedList.ConvertArrayToLinkedList('needle'), ),   -1),
-            ((LinkedList.ConvertArrayToLinkedList('stack'),     LinkedList.ConvertArrayToLinkedList('queue'), ),    -1),
+            (
+                (LinkedList.ConvertArrayToLinkedList('stack'),     
+                 LinkedList.ConvertArrayToLinkedList('queue'), ),    
+                 -1
+            ),
 
-            # Needle in the haystack. General.
-            ((LinkedList.ConvertArrayToLinkedList('hay'),       LinkedList.ConvertArrayToLinkedList('hay'), ),       0),
-            ((LinkedList.ConvertArrayToLinkedList('hhhay'),     LinkedList.ConvertArrayToLinkedList('hay'), ),       2),
-            ((LinkedList.ConvertArrayToLinkedList('saynay'),    LinkedList.ConvertArrayToLinkedList('nay'), ),       3),
-            ((LinkedList.ConvertArrayToLinkedList('deck'),      LinkedList.ConvertArrayToLinkedList('eck'), ),       1),
+            # Needle equal to the haystack.
+            (
+                (LinkedList.ConvertArrayToLinkedList('hay'),       
+                 LinkedList.ConvertArrayToLinkedList('hay'), ),      
+                 0
+            ),
 
-            # Needle in the haystack. Corner cases.
-            ((LinkedList.ConvertArrayToLinkedList('y'),         LinkedList.ConvertArrayToLinkedList('y'), ),         0),
-            ((LinkedList.ConvertArrayToLinkedList('hay'),       LinkedList.ConvertArrayToLinkedList('h'), ),         0),
-            ((LinkedList.ConvertArrayToLinkedList('bobob'),     LinkedList.ConvertArrayToLinkedList('bob'), ),       0),
-            ((LinkedList.ConvertArrayToLinkedList('loloroflo'), LinkedList.ConvertArrayToLinkedList('loro'), ),      2),
+            (
+                (LinkedList.ConvertArrayToLinkedList('hhhay'),     
+                 LinkedList.ConvertArrayToLinkedList('hay'), ),       
+                 2
+            ),
+            (
+                (LinkedList.ConvertArrayToLinkedList('saynay'),    
+                 LinkedList.ConvertArrayToLinkedList('nay'), ),       
+                 3
+            ),
+            (
+                (LinkedList.ConvertArrayToLinkedList('deck'),      
+                 LinkedList.ConvertArrayToLinkedList('eck'), ),       
+                 1
+            ),
+            (
+                (LinkedList.ConvertArrayToLinkedList('y'),         
+                 LinkedList.ConvertArrayToLinkedList('y'), ),         
+                 0
+            ),
+            (
+                (LinkedList.ConvertArrayToLinkedList('hay'),       
+                 LinkedList.ConvertArrayToLinkedList('h'), ),         
+                 0
+            ),
+            (
+                (LinkedList.ConvertArrayToLinkedList('bobob'),     
+                 LinkedList.ConvertArrayToLinkedList('bob'), ),       
+                 0
+            ),
+            (
+                (LinkedList.ConvertArrayToLinkedList('loloroflo'), 
+                 LinkedList.ConvertArrayToLinkedList('loro'), ),      
+                 2
+            ),
+
+            # Needle at the beginning.
+            (
+                (LinkedList.ConvertArrayToLinkedList('foobar'), 
+                 LinkedList.ConvertArrayToLinkedList('fo'), ),      
+                 0
+            ),
+
+            # Needle in the ending.
+            (
+                (LinkedList.ConvertArrayToLinkedList('foobar'), 
+                 LinkedList.ConvertArrayToLinkedList('ar'), ),      
+                 4
+            ),
+
+            # See: [href.] http://stackoverflow.com/questions/3134602/what-are-good-test-cases-for-benchmarking-stress-testing-substring-search-algo
+            #
+            # src = 'aabaabaabaabaabaabaab'
+            # pat = 'aaab'
+            #            
+            (
+                (LinkedList.ConvertArrayToLinkedList('aabaabaabaabaabaabaab'), 
+                 LinkedList.ConvertArrayToLinkedList('aaab'), ),      
+                 -1
+            ),
+
+            # See: [href.] http://stackoverflow.com/questions/3134602/what-are-good-test-cases-for-benchmarking-stress-testing-substring-search-algo
+            #
+            # src = 'yxyxyxxyxyxyxx'
+            # pat = 'yxyxyxxyxyxyxy'
+            #          
+            (
+                (LinkedList.ConvertArrayToLinkedList('yxyxyxxyxyxyxx'), 
+                 LinkedList.ConvertArrayToLinkedList('yxyxyxxyxyxyxy'), ),      
+                 -1
+            ),
         )
 
         areAllPassed = True
