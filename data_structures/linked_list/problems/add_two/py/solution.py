c519eb2a41068d889ce89ac27dd71f8369592475
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-03 00:25:44 +0200

    Update solution.py

--- data_structures/linked_list/problems/add_two/py/solution.py
+++ data_structures/linked_list/problems/add_two/py/solution.py
@@ -1,7 +1,7 @@
 import LinkedList
 
-# Problem description:      
-# Solution time complexity: 
+# Problem description:      Perform addition of two values, represented as two linked lists of their digits
+# Solution time complexity: O(n)
 # Comments:                 
 
 # Linked List Node inside the LinkedList module is declared as:
@@ -13,4 +13,45 @@ import LinkedList
 #
 
 def AddTwo(left: LinkedList.Node, right: LinkedList.Node) -> LinkedList.Node:
-    raise NotImplementedError()
+    def ListReverse(head: LinkedList.Node) -> LinkedList.Node:
+        prev = None
+        curr = head
+        
+        while curr != None:
+            nxt      = curr.nxt
+            curr.nxt = prev
+            prev     = curr
+            curr     = nxt
+
+        return prev
+
+    dummy     = LinkedList.Node(0)
+    dest      = dummy
+
+    left      = ListReverse(left)
+    right     = ListReverse(right)
+
+    leftNode  = left
+    rightNode = right
+
+    carry     = 0
+
+    while leftNode != None or rightNode != None:
+        leftVal  = leftNode.val  if leftNode  else 0
+        rightVal = rightNode.val if rightNode else 0
+
+        total    = carry + leftVal + rightVal
+        carry    = total // 10
+        addVal   = total % 10
+
+        dest.nxt = LinkedList.Node(addVal)
+        dest     = dest.nxt
+
+        if leftNode:  leftNode  = leftNode.nxt
+        if rightNode: rightNode = rightNode.nxt
+
+    if carry > 0:
+        dest.nxt = LinkedList.Node(carry)
+        dest     = dest.nxt
+
+    return ListReverse(dummy.nxt)
