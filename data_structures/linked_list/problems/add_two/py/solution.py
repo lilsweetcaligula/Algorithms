b3c41b18b0732a4a3f0ce9b7d15d94081aef1cf7
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-02 02:22:31 +0200

    Create solution.py

---
+++ data_structures/linked_list/problems/add_two/py/solution.py
@@ -0,0 +1,16 @@
+import LinkedList
+
+# Problem description:      
+# Solution time complexity: 
+# Comments:                 
+
+# Linked List Node inside the LinkedList module is declared as:
+#
+#   class Node:
+#       def __init__(self, val, nxt=None):
+#           self.val = val
+#           self.nxt = nxt
+#
+
+def AddTwo(left: LinkedList.Node, right: LinkedList.Node) -> LinkedList.Node:
+    raise NotImplementedError()
