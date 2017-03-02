b40eb5723eeab38edb2440d04d65f1c5be4ad4c0
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-02 02:46:25 +0200

    Create solution.py

---
+++ data_structures/linked_list/problems/anagrams/py/solution.py
@@ -0,0 +1,16 @@
+import LinkedList
+
+# Problem description:      
+# Solution time complexity: 
+# Comments:                 
+
+# Linked List Node inside the LinkedList module is declared as:
+#
+#   class Node:
+#       def __init__(self, val, nxt=None):
+#           self.val = val
+#           self.nxt = nxt
+#
+
+def AreAnagrams(left: LinkedList.Node, right: LinkedList.Node) -> bool:
+    raise NotImplementedError()
