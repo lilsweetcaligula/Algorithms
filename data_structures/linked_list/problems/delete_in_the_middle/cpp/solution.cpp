e49617b4e8f4cc11ef6316368e8e6db391331d1d
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-05 21:43:13 +0200

    Create solution.cpp

---
+++ data_structures/linked_list/problems/delete_in_the_middle/cpp/solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Problem description:      Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
+ * Solution time complexity: O(n)
+ * Comments:                 
+ */
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param node: a node in the list should be deleted
+     * @return: nothing
+     */
+    void deleteNode(ListNode *node) 
+    {
+        auto temp = node->next;
+        *node     = std::move(*node->next);
+        
+        delete temp; temp = nullptr;
+    }
+};
