eb6f63da13ac8f358422999b39eaa3b255c1b5b9
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-05 20:38:07 +0200

    Update solution.py

--- data_structures/linked_list/problems/anagrams/py/solution.py
+++ data_structures/linked_list/problems/anagrams/py/solution.py
@@ -1,7 +1,8 @@
 import LinkedList
 
-# Problem description:      
-# Solution time complexity: 
+# Problem description:      Given two strings represented as linked lists, determine 
+#                           if they are anagrams of each other.
+# Solution time complexity: O(n)
 # Comments:                 
 
 # Linked List Node inside the LinkedList module is declared as:
@@ -13,4 +14,32 @@ import LinkedList
 #
 
 def AreAnagrams(left: LinkedList.Node, right: LinkedList.Node) -> bool:
-    raise NotImplementedError()
+    import collections
+
+    if left == None or right == None:
+        return False
+
+    lnode  = left
+    rnode  = right
+    lookup = collections.Counter()
+    
+    while lnode != None and rnode != None:
+        lookup[lnode.val] += 1
+        lnode = lnode.nxt
+        rnode = rnode.nxt
+
+    # If either node is not None, means either
+    # list is bigger than the other. Thus, they
+    # cannot be anagrams of each other.
+    if lnode != None or rnode != None:
+        return False
+
+    rnode = right
+
+    while rnode != None:
+        if lookup[rnode.val] == 0:
+            return False
+        else:
+            lookup[rnode.val] -= 1
+
+    return True
