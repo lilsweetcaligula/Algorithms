f8166f8fc8fdc04ac40c0a7aec69fa948674f49b
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-05 23:01:25 +0200

    Update solution.py

--- data_structures/linked_list/problems/delete_n_after_m/py/solution.py
+++ data_structures/linked_list/problems/delete_n_after_m/py/solution.py
@@ -1,7 +1,8 @@
 import LinkedList
 
-# Problem description:      
-# Solution time complexity: 
+# Problem description:      Given a linked list, values n and m, remove n nodes after ever m nodes 
+#                           and return the resulting list.
+# Solution time complexity: O(n)
 # Comments:                 
 
 # Linked List Node inside the LinkedList module is declared as:
@@ -19,6 +20,13 @@ def DeleteNAfterMNodes(head: LinkedList.Node, n: int, m: int) -> LinkedList.Node
     slow = head
 
     while slow != None:
+        
+        # (m-1)th node will be the last node of the m subrange.
+        # (m)th node will be the first node in the n subrange, which
+        # is to be deleted.
+        #
+        # We need to land on the (m-1)th node in order to relink it to
+        # the next m subrange.        
         for _ in range(m - 1):
             if slow == None:
                 break
@@ -26,16 +34,22 @@ def DeleteNAfterMNodes(head: LinkedList.Node, n: int, m: int) -> LinkedList.Node
                 slow = slow.nxt
 
         if slow == None:
+            # If slow == None, we have reached the end of the list,
+            # and there's nothing left to delete...
             break
         else:
             fast = slow.nxt
 
             for _ in range(n):
                 if fast == None:
+                    # If fast == None, we have reached the end of the list,
+                    # and this is as far as we go for the current subrange.
                     break
                 else:
                     fast = fast.nxt
 
+            # Link the last node of the m subrange with the first node of
+            # the next m subrange. Move forward.
             slow.nxt = fast
             slow     = slow.nxt
 
