d37ad21830fc552a633022c2cf513b8df213c40e
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-05 23:04:03 +0200

    Update solution.cpp

--- data_structures/linked_list/problems/convert_sorted_to_bst/cpp/solution.cpp
+++ data_structures/linked_list/problems/convert_sorted_to_bst/cpp/solution.cpp
@@ -2,7 +2,7 @@
  * Problem description:      Given a singly linked list where elements are sorted in ascending order, convert 
  *                           it to a height balanced BST.
  * Solution time complexity: O(n log n)
- * Comments:                 Given solution is non-destructive to the list it's called on.
+ * Comments:                 Given solution is non-destructive to the list it's called upon.
  */
 
 /**
