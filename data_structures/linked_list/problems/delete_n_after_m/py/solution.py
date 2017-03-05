356849b300c01f5c089b1a76e39efad03d2f29d6
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-05 23:03:31 +0200

    Update solution.py

--- data_structures/linked_list/problems/delete_n_after_m/py/solution.py
+++ data_structures/linked_list/problems/delete_n_after_m/py/solution.py
@@ -3,7 +3,11 @@ import LinkedList
 # Problem description:      Given a linked list, values n and m, remove n nodes after ever m nodes 
 #                           and return the resulting list.
 # Solution time complexity: O(n)
-# Comments:                 
+# Comments:                 See the comments in the source code. The tricky part is to realize that
+#                           we need to stop the traversal of the m subrange on the (m-1)th node, as
+#                           opposed to the mth node, which will incidentally be the first node of
+#                           the n subrange. We need this in order to link up the last node in the
+#                           current m subrange with the next m subrange.
 
 # Linked List Node inside the LinkedList module is declared as:
 #
