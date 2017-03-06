72d0563cd3a56ef10b446df65046144d5ce87268
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-06 19:46:11 +0200

    Create solution.cpp

---
+++ data_structures/linked_list/problems/find_nth_to_the_last/cpp/solution.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem description:      Find the nth to last element of a singly linked list.
+ *                           The minimum number of nodes in list is n.
+ * Solution time complexity: O(n)
+ * Comments:                 
+ */
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer.
+     * @return: Nth to last node of a singly linked list. 
+     */
+    ListNode *nthToLast(ListNode *head, int n) 
+    {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        
+        auto slow = head;
+        auto fast = head;
+        
+        for (auto i = n; i > 0; --i) {
+            fast = fast->next;
+        }
+        
+        while (fast != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        return slow;
+    }
+};
