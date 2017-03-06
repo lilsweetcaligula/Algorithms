fbca0b7d939191bc170299b579fd531be85f839d
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-06 21:51:22 +0200

    Update solution2.py

--- data_structures/linked_list/problems/find_merge_point/py/solution2.py
+++ data_structures/linked_list/problems/find_merge_point/py/solution2.py
@@ -2,6 +2,22 @@
 #                           no intersection, return null.
 # Solution time complexity: O(n)
 # Comments:                 The solution with a lookup table - extra memory.
+#
+#                               a1->a2
+#                                     \
+#                                      c1->c2->c3 
+#                                     /
+#                           b1->b2->b3
+#
+#                           lookup = { 
+#                               b1, b2, b3, c1, c2, c3
+#                           }
+#
+#                           a1,         a1 in lookup? False
+#                           a1->a2,     a2 in lookup? False
+#                           a1->a2->c1, c1 in lookup? True, c1 == intersection node
+#
+#
 
 # Definition for singly-linked list.
 # class ListNode(object):
