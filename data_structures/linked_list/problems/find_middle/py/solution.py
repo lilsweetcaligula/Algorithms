4ef8fdb862c2e32a338f5ae412de6efcba91137d
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-06 21:21:22 +0200

    Update solution.py

--- data_structures/linked_list/problems/find_middle/py/solution.py
+++ data_structures/linked_list/problems/find_middle/py/solution.py
@@ -3,6 +3,23 @@ import LinkedList
 # Problem description:      Given a linked list, find its middle node.
 # Solution time complexity: O(n)
 # Comments:                 Slow/fast pointer technique.
+#                           
+#                           i)  0->1->2->3->4->5
+#                           ii) 0->1->2->3->4->5
+#                               S
+#                               F
+#
+#                           iii) o->S
+#                                o->x->F
+#
+#                            iv) o->o->S
+#                                o->x->o->x->F
+#
+#                             v) o->o->o->S
+#                                o->x->o->x->o->x->F
+#
+#                                M == S
+#                               
 
 # Linked List Node inside the LinkedList module is declared as:
 #
